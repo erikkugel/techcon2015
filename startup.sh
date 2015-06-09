cc4692e8f719ffccf188ded2cd7962215d94955e
erikkugel <erik.kugel@gmail.com>
2015-06-09 13:07:30 -0400

    Verbose output and SNMP

--- startup.sh
+++ startup.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
 
+echo "Start ElasticSearch"
 service elasticsearch start
 sleep 5
 
+echo "Start Logstash server"
 service logstash start
 sleep 5
 
+echo "Start Logstash Forwarder"
 service logstash-forwarder start
 sleep 5
 
-su - techcon -c "cd /opt/problem3; python bin/problem3.py & echo $! > /tmp/problem3.pid"
+echo "Start SNMP server"
+service snmpd start 
+sleep 5
 
+echo "Start Problem #3 web app."
+su - techcon -c "cd /opt/problem3; python bin/problem3.py &"
+sleep 5
+
+echo "Start loading Problem #3:"
 while [ 1 ]; do wget -q -O - http://ubuntu:8080/ ; sleep 3; date; done
